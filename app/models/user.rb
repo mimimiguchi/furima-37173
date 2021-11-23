f9b643cd64db46ac02b0f6362f1ea2fc87010ce2
mimimiguchi <mimimiguchi@icloud.com>
2021-11-24 08:34:06 +0900

    バリデーションの追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+        
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is valid. Include both letters and numbers'}
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  
+  
 
 end
