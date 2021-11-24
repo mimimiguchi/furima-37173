60781889b474f1dd06131ebc721946c7928d6a87
mimimiguchi <mimimiguchi@icloud.com>
2021-11-24 20:53:10 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name, :description, :category_id, :status_id, :delivery_charge_id, presence: true
+  validates :prefecture_id, :delivery_day_id, :price, :image, presence: true
+
 end
