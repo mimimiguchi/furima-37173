38c95195cfbe5e2afb9b04b5ed6efb2d6c330445
mimimiguchi <mimimiguchi@icloud.com>
2021-11-25 19:56:40 +0900

    Itemモデルの単体テスト実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,8 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }, numericality: {in: 300..9999999, message: 'is out of setting range'}
+  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } 
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range'}
   validates :category_id, numericality: {other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: {other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id, numericality: {other_than: 1 , message: "can't be blank"}
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name             {'あ'}
+    description           {Faker::Lorem.sentence}
+    category_id           {2}
+    status_id             {2}
+    delivery_charge_id    {2}
+    prefecture_id         {2}
+    delivery_day_id       {2}
+    price                 {300}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/nas.jpg'), filename: 'nas.jpg')
+    end
+
   end
 end
