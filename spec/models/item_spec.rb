66c4d40dfa7e5607dfee5b70c6daf606009b1b8a
mimimiguchi <mimimiguchi@icloud.com>
2021-11-25 20:36:02 +0900

    レビュー修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -86,6 +86,12 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Image can't be blank")
      end
      
+     it 'userが紐ついていないと出品できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
+     
     end
     
   end
