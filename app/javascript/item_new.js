cd771060b66e1a4a948f0de8044c7be0339d5a2a
mimimiguchi <mimimiguchi@icloud.com>
2021-11-25 16:08:49 +0900

    商品出品機能の実装

---
+++ app/javascript/item_new.js
@@ -0,0 +1,12 @@
+function cal (){
+  const price = document.getElementById("item-price")
+  price.addEventListener('keyup', function(){
+    const priceVal = price.value
+    const addTaxPrice = document.getElementById("add-tax-price")
+    addTaxPrice.innerHTML =  Math.floor( price.value * 0.1 )
+    const profit = document.getElementById("profit")
+    profit.innerHTML = price.value - Math.floor( price.value * 0.1 )
+  })
+}
+
+window.addEventListener('load', cal)
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :item_name, :description, :category_id, :status_id, :delivery_charge_id, presence: true
-  validates :prefecture_id, :delivery_day_id, :price, :image, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }, numericality: {in: 300..9999999, message: 'is out of setting range'}
+  validates :category_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id, numericality: {other_than: 1 , message: "can't be blank"}
 
 end
