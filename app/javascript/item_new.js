b12d6d1afd14b6e222f999b1cacfd4d0907077c3
mimimiguchi <mimimiguchi@icloud.com>
2021-12-01 13:36:17 +0900

    販売手数料・利益の計算式変更

--- app/javascript/item_new.js
+++ app/javascript/item_new.js
@@ -5,7 +5,7 @@ function cal (){
     const addTaxPrice = document.getElementById("add-tax-price")
     addTaxPrice.innerHTML =  Math.floor( price.value * 0.1 )
     const profit = document.getElementById("profit")
-    profit.innerHTML = price.value - Math.floor( price.value * 0.1 )
+    profit.innerHTML = Math.ceil( price.value - price.value * 0.1 )
   })
 }
 
